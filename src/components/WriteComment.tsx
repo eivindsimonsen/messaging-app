283a1726d130090fa85cfb666c64cfac11b3d485
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-04-05 12:20:54 +0200

    Send message comp complete

--- src/components/WriteComment.tsx
+++ src/components/WriteComment.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import image from "../assets/image-maxblagun.png";
 
 function WriteComment() {
-  return <div className="write-comment">WriteComment</div>;
+  return (
+    <form className="write-comment">
+      <img
+        src={image}
+        alt=""
+      />
+      <textarea
+        name="reply"
+        rows={4}
+        placeholder="Replying to username.."></textarea>
+      <button className="cta">SEND</button>
+    </form>
+  );
 }
 
 export default WriteComment;
